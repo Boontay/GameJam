983d064c747d131965b3c5251b304e99a8d2267a
Kamiom <44639559+Kamiom@users.noreply.github.com>
2018-11-03 13:03:44 +0000

    Add files via upload

---
+++ Assets/Scripts/Lighting.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lighting : MonoBehaviour {
+	public float speed = 1;
+	public bool lit = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 pos = GameObject.Find("Player").transform.position;
+		pos = new Vector3 (pos.x, pos.y, pos.z -1);
+		transform.position = pos;
+
+	}
+}
