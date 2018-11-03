99ab01da065c0b34e609ddce750861be98935a8c
Boontay <36228019+Boontay@users.noreply.github.com>
2018-11-03 12:22:14 +0000

    Add files via upload

---
+++ Assets/Scripts/DisplayHealthBar.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayHealthBar : MonoBehaviour {
+    public Transform player;
+    public Text healthText;
+
+    private void Update() {
+        healthText.text = player.position.z.ToString("0");
+    }
+}
